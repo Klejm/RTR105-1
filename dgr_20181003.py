c3deaefbdd07e13c83aa1d104a99f9d61cc38575
user <machoeagle@gmail.com>
2018-10-03 09:32:33 +0300

    history_20181003.txt

---
+++ dgr_20181003.py
@@ -0,0 +1,11 @@
+Python 2.7.12 (default, Dec  4 2017, 14:50:18) 
+[GCC 5.4.0 20160609] on linux2
+Type "copyright", "credits" or "license()" for more information.
+>>> print("hello world")
+hello world
+==================== RESTART: /home/user/RTR105/commands_20181003 ====================
+hello world
+Enter Hours: 35
+Enter Rate: 2.75
+('Your Pay: ', 96.25)
+>>> 
